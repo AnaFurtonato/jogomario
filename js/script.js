2ed613f7e73537c8b1ea6617f4cf077af599092b
Ana Clara Furtonato <anafortunato44@gmail.com>
2022-09-27 22:56:50 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,6 +25,10 @@ const loop = setInterval(()=>{
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`
 
+        mario.src = '../img/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';   
+        
 
 
         clearInterval(loop);
